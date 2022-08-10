331d11540e14d0f0158525b2ad4b02af3d9c9a59
Moontoast435 <seanmalone1993@gmail.com>
2022-08-10 14:08:41 +0100

    pre-merge commit

--- github-clone/src/index.js
+++ github-clone/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles';
 import App from './App';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from './store';
+// import store from './store';
 
 ReactDOM.render(
   <BrowserRouter>
-  <Provider store ={store}>
+
       <App />
-  </Provider>
+  
   </BrowserRouter>,
   document.getElementById('root')
 )
--- github-clone/src/store.js
+++ github-clone/src/store.js
@@ -2,8 +2,8 @@ import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { allReducers } from './reducers';
+//import { allReducers } from './reducers';
 
-const store = createStore(allReducers, composeWithDevTools(applyMiddleware(thunk)));
+// const store = createStore(allReducers, composeWithDevTools(applyMiddleware(thunk)));
 
-export default store;
+// export default store;
