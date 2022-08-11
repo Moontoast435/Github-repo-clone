2365a40b4993129094632687a0f722493cb760f4
Moontoast435 <seanmalone1993@gmail.com>
2022-08-11 10:43:28 +0100

    pre-merge commit

--- github-clone/src/containers/ProductComponent.js
+++ github-clone/src/containers/ProductComponent.js
@@ -3,30 +3,24 @@ import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
-  const [showText, setShowText] = useState(false);
-  const yo = () => {
-    console.log('clicked');
-    setShowText(!showText);
+  const [showText, setShowText] = useState('');
+  const handleClick = (id) => {
+    setShowText(id);
   };
-  const renderList = products.map((product, index) => {
+  const renderList = products.map((product,index) => {
     const {
       id,
-      title,
-      price,
-      category,
-      image,
       full_name,
       forks_count,
       open_issues,
       stargazers_count,
     } = product;
-    console.log(product);
     return (
-      <div key={index}>
-        <h1 style={{ cursor: 'pointer' }} onClick={yo}>
+      <div key={id}>
+        <h1 style={{ cursor: 'pointer' }} onClick={() => handleClick(index)}>
           {full_name}
         </h1>
-        {showText && (
+        {showText === index && (
           <div>
             Forks Count: {forks_count} <br></br> Open Issues: {open_issues}
             <br />
