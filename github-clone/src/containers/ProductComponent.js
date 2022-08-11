313b73908f53979475bb452dca438370cb3da24d
Moontoast435 <seanmalone1993@gmail.com>
2022-08-11 08:38:47 +0100

    matching sam's work

--- github-clone/src/containers/ProductComponent.js
+++ github-clone/src/containers/ProductComponent.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
-  const renderList = products.map((product) => {
-    const { id, stargazers_count, open_issues, forks, name } = product;
+  const [showText, setShowText] = useState(false);
+  const yo = () => {
+    console.log('clicked');
+    setShowText(!showText);
+  };
+  const renderList = products.map((product, index) => {
+    const {
+      id,
+      title,
+      price,
+      category,
+      image,
+      full_name,
+      forks_count,
+      open_issues,
+      stargazers_count,
+    } = product;
     console.log(product);
     return (
-      <div key={id}>
-        <h1>Name of Repo: {name}</h1>
-         <p>Number of forks: {forks}</p>
-        <p>Stargazers: {stargazers_count}</p>
-        <p>Issues: {open_issues}</p> 
+      <div key={index}>
+        <h1 style={{ cursor: 'pointer' }} onClick={yo}>
+          {full_name}
+        </h1>
+        {showText && (
+          <div>
+            Forks Count: {forks_count} <br></br> Open Issues: {open_issues}
+            <br />
+            Total Stars: {stargazers_count}
+          </div>
+        )}
       </div>
     );
   });
-  // const { id, title } = products[0];
   return <div>{renderList}</div>;
 };
 
